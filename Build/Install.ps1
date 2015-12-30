4391998501defc810d8989908eb57bdfcffd04df
Piers Karsenbarg <piers.karsenbarg@t101.com>
2015-12-30 13:10:53 +0000

    Finalised install.ps1 so it generates a key and adds it to the config

--- Build/Install.ps1
+++ Build/Install.ps1
@@ -6,7 +6,7 @@ function GenerateKey()
 }
 
 
-function UpdateCurrentProjectsConfigFile([string]$name)
+function UpdateCurrentProjectsConfigFile()
 {
     $config = $project.ProjectItems | where {$_.Name -eq "Web.config"}
     if ($config -eq $null)
@@ -18,28 +18,23 @@ function UpdateCurrentProjectsConfigFile([string]$name)
         }
     }
     $localPath = $config.Properties | where {$_.Name -eq "LocalPath"}
-    UpdateConfigFile($localPath.Value, $name)
+    UpdateConfigFile $localPath.Value
 }
 
-function UpdateConfigFile([string]$configFilePath, [string]$name)
+function UpdateConfigFile($configFilePath)
 {
-Write-Host "configFilePath: " 
-Write-Host $configFilePath
-	$references = (Select-String $configFilePath -pattern "TinySharpKey").Matches.Count
+    $tinyKey = GenerateKey
+    $xml = New-Object XML
+    $xml.Load($configFilePath)
+	$appSettingNode = $xml.SelectSingleNode("configuration/appSettings/add[@key = 'TinySharpKey']")
 
-	If( $references -ne 0)
+    Write-Host $appSettingNode
+
+	If( $appSettingNode -ne $null)
 	{
 		Write-Host "TinySharpKey already exists in config file"
 		return
-	}
-
-	$xml = New-Object xml
-    $xml.Load($configFilePath)
-
-	$appSettingNode = $xml.SelectSingleNode("configuration/appSettings/add[@key = 'TinySharpKey']")
-
-	
-	Write-Host "Adding TinySharpKey appSetting to " $configFilePath
+	}	
 
     $appSettingsNode = $xml.SelectSingleNode("configuration/appSettings")
     if ($appSettingsNode -eq $null)
@@ -48,14 +43,22 @@ Write-Host $configFilePath
         $xml.DocumentElement.AppendChild($appSettingsNode)
     }
 
-    if ($name -eq "")
+    if ($tinyKey -ne "")
     {    
         $comment = $xml.CreateComment("TinySharpKey is used to generate the hash. Do not change it once you start using TinySharp or you won't be able to reverse any previously generated hashes.")
         $appSettingsNode.AppendChild($comment)
+    }   
+
+    if($appSettingNode -eq $null)
+    {
+        $keyElement = $xml.CreateElement("add")
+        $keyElement.SetAttribute("key", "TinySharpKey")
+        $keyElement.SetAttribute("value", "$tinyKey")
+        $appSettingsNode.AppendChild($keyElement)
     }
+
     
     $xml.Save($configFilePath)
 }
 
-$key = GenerateKey
-UpdateCurrentProjectsConfigFile $key
+UpdateCurrentProjectsConfigFile
