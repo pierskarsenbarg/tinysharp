7e4c1a51b0702c71cdb5a35c4cc55706d6e1a6f3
Piers Karsenbarg <piers.karsenbarg@t101.com>
2015-12-30 14:35:13 +0000

    Renamed test class

---
+++ TinySharp.Tests/TinySharpTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+
+namespace TinySharp.Tests
+{
+    [TestFixture]
+    public class TinySharpTests
+    {
+        private string _key;
+        private Tiny _tiny;
+
+        [SetUp]
+        public void Setup()
+        {
+            _key = Tiny.GenerateKey();
+            _tiny = new Tiny(_key);
+        }
+
+        [Test]
+        public void ToHash_ReturnsString()
+        {
+            Assert.IsInstanceOf<string>(_tiny.ToHash(1));
+        }
+
+        [Test]
+        public void ReverseHash_ReturnsInt()
+        {
+            var hash = _tiny.ToHash(1);
+            Assert.IsInstanceOf<int>(_tiny.ReverseHash(hash));
+        }
+
+        [Test]
+        public void ReverseHash_ReturnsCorrectValue()
+        {
+            const int id = 1234;
+            var hash = _tiny.ToHash(id);
+            Assert.AreEqual(id, _tiny.ReverseHash(hash));
+        }
+
+        [Test]
+        public void ToHash_ReturnsCorrectValue()
+        {
+            const int id = 1;
+            var hash = _tiny.ToHash(id);
+            Assert.AreEqual(_key.Substring(1, 1), hash);
+        }
+    }
+}
